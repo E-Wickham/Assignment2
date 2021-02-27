c9f5554a78790552a3b8b71dafad6da8b87caac0
E-Wickham <ericwickham@hotmail.com>
2021-02-26 22:19:47 -0500

    Add project files.

---
+++ Assignment2/Controllers/DiceController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment2.Controllers
+{
+    public class DiceController : ApiController
+    {
+        [HttpGet]
+        [Route("api/J2/DiceGame/{m}/{n}")]
+
+        public int Dice(m, n)
+        {
+            /// add all possible combinations of the two die with a for loop, then with an if statement, 
+            /// have it count the number of times the sum of the two die equal ten
+            ///
+            /// I don't know if this code would actually work, but this is essentially what I think I would do for a problem like this. 
+            /// Take the inputs from the user, use for loops to create every possible combination, then find the ones that sum to 10, 
+            /// and have a variable counting the number of times you reach that sum
+            /// Then at the very end, you return a statement saying "There are " + GoodRoll + " total ways to get the sum 10."
+            ///
+            /// GoodRoll = 0
+            ///
+            /// for (int i=1, i<=m, i++) {
+            ///     for (int i2=1, i2<=n, i2++) {
+            ///  if(i + i2 = 10) {
+            ///    GoodRoll += 1
+            ///     }
+            /// 
+            /// return string "There are " + GoodRoll + " total ways to get the sum 10."
+            /// 
+            ///<summary>
+            ///Calculates all possible combinations of dice rolls to get 10 
+            ///</summary>
+            ///<param name="m">Die 1</param>
+            ///<param name="n">Die 2</param>
+            ///<example> api/J2/DiceGame/2/2 -> 0</example>
+            ///<example> api/J2/DiceGame/5/5 -> 1</example> 
+            ///<returns>The total number of the </returns>
+
+        }
+
+
+    }
+}
---
+++ Assignment2/Controllers/J1Controller.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment2.Controllers
+{
+
+    public class J1Controller : ApiController
+    {
+          
+        [HttpGet]
+        [Route("api/J1/Menu/{Burger}/{Drink}/{side}/{dessert}")]
+        public int GetMenu (int Burger, int Drink, int side, int dessert) 
+        {
+            ///int Calories = 0;
+            ///if statement to determine amount of calories, if less than two, its a cheeseburger, 
+            ///if it is less than three -> fish burger, less than 4 veggie, 4 or higher -> no burger
+            ///since using if burger = 1 gives an error, using if statements in this fashion will remove any potential issues with the menu
+            
+            if (Burger < 1) { 
+                Burger = 0;
+            } else if (Burger < 2) {
+                Burger = 461;
+            } else if (Burger < 3) {
+                Burger = 431;
+            } else if (Burger < 4) {
+                Burger = 420;
+            } else {
+                Burger = 0;
+            }
+
+            // Drink If statement
+
+            if (Drink < 1)
+            {
+                Drink = 0;
+            }
+            else if (Drink < 2)
+            {
+                Drink = 130;
+            }
+            else if (Drink < 3)
+            {
+                Drink = 160;
+            }
+            else if (Drink < 4)
+            {
+                Drink = 118;
+            }
+            else
+            {
+                Drink = 0;
+            }
+
+            // Side Order If statement
+
+            if (side < 1)
+            {
+                side = 0;
+            }
+            else if (side < 2)
+            {
+                side = 100;
+            }
+            else if (side < 3)
+            {
+                side = 57;
+            }
+            else if (side < 4)
+            {
+                side = 70;
+            }
+            else
+            {
+                side = 0;
+            }
+
+            // dessert Order If statement
+
+            if (dessert < 1)
+            {
+                dessert = 0;
+            }
+            else if (dessert < 2)
+            {
+                dessert = 167;
+            }
+            else if (dessert < 3)
+            {
+                dessert = 266;
+            }
+            else if (dessert < 4)
+            {
+                dessert = 75;
+            }
+            else
+            {
+                dessert = 0;
+            }
+
+            return Burger + Drink + side + dessert;
+
+            ///<summary>
+            ///Calculates calories of the meal based on the input from the user
+            ///</summary>
+            ///<param name="Burger">First Integer Factor</param>
+            ///<param name="Drink">Second Integer Factor</param>
+            ///<param name="side">First Integer Factor</param>
+            ///<param name="dessert">Second Integer Factor</param>
+            ///<example> api/J1/menu/1/1/1/1 -> 858</example>
+            ///<example> api/J1/menu/2/2/2/2 -> 914</example> 
+            ///<returns>The sum of the selected items</returns>
+        }
+
+    }
+}
